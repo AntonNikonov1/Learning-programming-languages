8c95f6122ee2833165caec65c3314a988ca82da0
AntonNikonov1 <antonanton@MacBook-Pro-Anton.local>
2021-04-05 15:32:37 +0300

    hw

--- Js/adding-two-numbers-via-url/index.js
+++ Js/adding-two-numbers-via-url/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/:numOne/:numTwo', (req, res) => {
-  const { numOne, numTwo } = req.params
-  const sum = Number(numOne) + Number(numTwo)
-
-  res.send(`<p>${sum}</p>`)
+app.get('/', (req) => {
+  console.log(req.query)
 });
 
 
@@ -17,3 +14,5 @@ app.listen(3000, () => {
 // Explanation
 // 1) to download all packages, enter the command in the terminal "npm install"
 // 2) in the terminal, type the command "node index"
+// 3) http://localhost:3000/?key1=value1?key2=value2
+// 4) in the editor console you will see the object
